f4d5a4b1792d998592aa2fae94b38e92511484eb
Fahad_Kader <ashikulf@gmail.com>
2022-01-21 03:41:53 -0500

    initial

---
+++ Selenium_Introduction/src/main/java/SeleniumIntroduction.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumIntroduction {
+    public static void main(String[] args) {
+
+        // 1st we have to invoke browser.
+        // Chrome - ChromeDriver -> Methods
+
+
+        ChromeDriver driver = new ChromeDriver();
+    }
+}
