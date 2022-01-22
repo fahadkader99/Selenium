0998070988c3904109441278c67716ca2e063e9a
Fahad_Kader <ashikulf@gmail.com>
2022-01-21 21:33:46 -0500

    first lessopn

---
+++ Selenium_Introduction/src/main/java/browserSetUp/SeleniumIntroSafari.java
@@ -0,0 +1,18 @@
+package browserSetUp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class SeleniumIntroSafari {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.safari.driver","/usr/bin/safaridriver");
+        WebDriver driver = new SafariDriver();
+        driver.get("https://www.rahulshettyacademy.com/");
+        System.out.println("Title: "+ driver.getTitle());
+        System.out.println("Current Url: "+ driver.getCurrentUrl());
+        System.out.println("Last page loaded "+ driver.getPageSource());
+        driver.close();
+    }
+}
